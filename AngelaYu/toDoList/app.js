ad92630662c9d2dff9b7d6e1ff23e19b64f55725
cuongpct109 <cuongpct109@gmail.com>
2022-02-08 20:03:35 +0700

    chen css vao list.ejs

--- AngelaYu/toDoList/app.js
+++ AngelaYu/toDoList/app.js
@@ -8,6 +8,8 @@ let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
