552463f4d9df26914557ee5cf6502b7015ad8055
cuongpct109 <cuongpct109@gmail.com>
2022-02-09 10:25:25 +0700

    hoan thanh toDoList project

--- AngelaYu/toDoList/app.js
+++ AngelaYu/toDoList/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -15,15 +16,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "short",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  let day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
---
+++ AngelaYu/toDoList/date.js
@@ -0,0 +1,25 @@
+function getDate() {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "short",
+  };
+
+  return (day = today.toLocaleDateString("en-US", options));
+}
+
+// module.exports.getDate = getDate;
+exports.getDate = getDate;
+
+function getDay() {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+  };
+
+  return (day = today.toLocaleDateString("en-US", options));
+}
+
+// module.exports.getDay = getDay;
+exports.getDay = getDay;
