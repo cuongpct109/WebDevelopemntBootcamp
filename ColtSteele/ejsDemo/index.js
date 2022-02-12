fa76275f228dfe659e62e43060a220c5c473dd4c
cuongpct109 <cuongpct109@gmail.com>
2022-02-12 20:56:58 +0700

    them ejsDemo

---
+++ ColtSteele/ejsDemo/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const redditData = require("./data.json");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "/views"));
+// app.get("/", (req, res) => {
+//   res.render("home");
+// });
+
+app.get("/rand", (req, res) => {
+  const randNum = Math.floor(Math.random() * 10) + 1;
+  res.render("home", { randNum: randNum });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  let { subreddit } = req.params;
+
+  let data = redditData[subreddit];
+
+  if (data) {
+    res.render("subreddit", { ...data });
+  } else {
+    res.render("notFound", { subreddit });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("LISTENING ON PORT 3000");
+});
