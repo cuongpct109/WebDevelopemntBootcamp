03fbd0c87af88ca1514e4018f91c175c9fc3da80
cuongpct109 <cuongpct109@gmail.com>
2022-02-17 17:32:28 +0700

    chinh sua todoList project voi mongoose

--- AngelaYu/toDoList/app.js
+++ AngelaYu/toDoList/app.js
@@ -1,35 +1,58 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+// let items = ["Buy Food", "Cook Food", "Eat Food"];
 
-let workItems = ["first work"];
+// let workItems = ["first work"];
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
-
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!",
 });
 
+const item2 = new Item({
+  name: "Hit the + button to add a new item.",
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item.",
+});
+
+const defaultItems = [item1, item2, item3];
+
+app.get("/", function (req, res) {
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        err ? console.log(err) : console.log("");
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
+});
+
+// =====================================================================================
+
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
-  console.log(req.body);
-  if (req.body.list === "Work List") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  let newItemName = req.body.newItem;
+  Item.insertMany([{ name: newItemName }]);
+  res.redirect("/");
 });
 
 app.get("/work", function (req, res) {
--- AngelaYu/toDoList/date.js
+++ AngelaYu/toDoList/date.js
@@ -1,25 +1,25 @@
-function getDate() {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "short",
-  };
+// function getDate() {
+//   let today = new Date();
+//   let options = {
+//     weekday: "long",
+//     day: "numeric",
+//     month: "short",
+//   };
 
-  return (day = today.toLocaleDateString("en-US", options));
-}
+//   return (day = today.toLocaleDateString("en-US", options));
+// }
 
-// module.exports.getDate = getDate;
-exports.getDate = getDate;
+// // module.exports.getDate = getDate;
+// exports.getDate = getDate;
 
-function getDay() {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-  };
+// function getDay() {
+//   let today = new Date();
+//   let options = {
+//     weekday: "long",
+//   };
 
-  return (day = today.toLocaleDateString("en-US", options));
-}
+//   return (day = today.toLocaleDateString("en-US", options));
+// }
 
-// module.exports.getDay = getDay;
-exports.getDay = getDay;
+// // module.exports.getDay = getDay;
+// exports.getDay = getDay;
