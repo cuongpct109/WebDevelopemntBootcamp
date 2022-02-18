a746fcf5301fc516e14cc9b5de1aab1519a2bd57
cuongpct109 <cuongpct109@gmail.com>
2022-02-18 14:29:20 +0700

    xoa blogWebsite project, them blogWithDatabase project

--- AngelaYu/Blog-with-Database/app.js
+++ AngelaYu/Blog-with-Database/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
+const path = require("path");
+const { stringify } = require("querystring");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -10,58 +11,87 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
-
 const app = express();
 
+// Views folder and EJS setup:
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+//To parse form data in POST request body:
+app.use(express.urlencoded({ extended: true }));
+
+// To parse incoming JSON in POST request body:
+app.use(express.json());
+
+// Public folder setup:
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect("mongodb://localhost:27017/postDB");
+
+const postSchema = mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts,
+  Post.find({}, (err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts,
+      });
+    }
   });
 });
 
-app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
-});
-
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
-});
-
-app.get("/compose", function (req, res) {
-  res.render("compose");
-});
-
 app.post("/compose", function (req, res) {
-  let postTitle = req.body.postTitle;
+  let post = new Post({
+    title: _.trim(_.capitalize(req.body.postTitle)),
+    content: req.body.postBody,
+  });
 
-  let postBody = req.body.postBody;
-
-  let post = {
-    title: postTitle,
-    content: postBody,
-  };
-
-  posts.push(post);
-
-  res.redirect("/");
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    } else {
+      console.log(`errors at /compose post request: ${err}`);
+    }
+  });
 });
 
 app.get("/posts/:postName", function (req, res) {
-  let postName = req.params.postName;
-  let postNameLowerCase = _.lowerCase(postName.toLowerCase());
+  const requestedTitle = req.params.postName;
 
-  for (let p of posts) {
-    if (postNameLowerCase === _.lowerCase(p.title.toLowerCase())) {
-      res.render("post", { postTitle: p.title, postContent: p.content });
+  Post.findOne({ title: req.params.postName }, (err, post) => {
+    if (!err) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    } else {
+      console.log(`errors at /posts/:postName: ${err}`);
     }
-  }
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.get("*", (req, res) => {
+  res.send(`<h2>404! Page is not found.</h2>`);
 });
 
 app.listen(3000, function () {
