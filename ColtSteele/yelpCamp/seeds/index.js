b5cf0dc26a1017f0e64821f3d777c7a6af74011e
cuongpct109 <cuongpct109@gmail.com>
2022-02-21 12:51:09 +0700

    update yelpCamp project

---
+++ ColtSteele/yelpCamp/seeds/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { descriptors, places } = require("./seedHelpers");
+
+mongoose.connect("mongodb://localhost:27017/yelpcamp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const camp = new Campground({
+      location: `${sample(cities).city}, ${sample(cities).state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
