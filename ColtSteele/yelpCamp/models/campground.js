cbe4e0772564e8bd4f2ff135a2a010c46a84fc24
cuongpct109 <cuongpct109@gmail.com>
2022-02-23 14:42:49 +0700

    chinh sua yelpcamp

--- ColtSteele/yelpCamp/models/campground.js
+++ ColtSteele/yelpCamp/models/campground.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-  title: String,
-  price: String,
-  description: String,
-  image: String,
-  location: String,
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  location: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
