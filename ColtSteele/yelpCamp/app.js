549cfe391878f4a36fca9711587254b7d9bedd1d
cuongpct109 <cuongpct109@gmail.com>
2022-02-26 16:15:36 +0700

    dang them basic authen cho yelpcamp

--- ColtSteele/yelpCamp/app.js
+++ ColtSteele/yelpCamp/app.js
@@ -5,12 +5,16 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
+
+const User = require("./models/user");
 
 mongoose.connect("mongodb://localhost:27017/yelpcamp");
 const db = mongoose.connection;
@@ -43,6 +47,14 @@ app.use(
 );
 
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
